abb081b00f664a296d8cade2fb438209cb715cd9
Bogdan Teodoru <me@bogdanteodoru.com>
2022-10-14 14:29:52 +0300

    Update setup.js

--- setup.js
+++ setup.js
@@ -20,7 +20,7 @@ var execute = function(pathParts, params, callback) {
 if (mode === 'postinstall') {
   fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
     if (exists) return;
-    execute(['node_modules', 'coffee-script', 'bin', 'coffee'], '-o lib/ src/');
+    execute(['node_modules', 'coffeescript', 'bin', 'coffee'], '-o lib/ src/');
   });
 } else if (mode === 'test') {
   execute(['node_modules', 'mocha', 'bin', 'mocha'],
